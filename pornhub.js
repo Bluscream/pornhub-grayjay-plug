bc68a110c3c1c3d47d29cd983a08f9fbafe11490
Adamsmashem <68350584+Adamsmashem@users.noreply.github.com>
2023-10-29 21:10:03 -0600

    Update pornhub.js

--- pornhub.js
+++ pornhub.js
@@ -7,7 +7,7 @@
 	
 	"sourceUrl": "https://github.com/Adamsmashem/hub-grayjay-plug",
 	"repositoryUrl": "https://github.com/Adamsmashem/hub-grayjay-plug",
-	"scriptUrl": "https://raw.githubusercontent.com/Adamsmashem/hub-grayjay-plug/main/hub2.js",
+	"scriptUrl": "https://raw.githubusercontent.com/Adamsmashem/hub-grayjay-plug/main/hub.js",
 	"version": 1,
 	"iconUrl": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.explicit.bing.net%2Fth%3Fid%3DOIP.wP9gBGh4DHKOHA8jQg34CQHaGM%26pid%3DApi&f=1&ipt=9d7e6e939de5d0b1388692c4e1b6b1c22fc2439b2a0a0c604b76f48adfb23910&ipo=images",
 	"id": "c215cb1a-6f06-4bfe-985f-d481e590f1ab",
